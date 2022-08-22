f9d07ff5f57bc8bcfe41da6d301965e4b8404ea3
ShadePrima <aleksandrag1984@gmail.com>
2022-08-22 17:50:45 +0300

    add marker

---
+++ src/components/RouteMap/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { Image, View } from "react-native";
+import MapViewDirections from "react-native-maps-directions";
+
+const GOOGLE_MAPS_APIKEY = "AIzaSyAFZdRRBDQVvcbi8FT4_HQfCqVPy6T6NVo";
+
+const RouteMap = () => {
+  const origin = { latitude: 47.757424, longitude: 35.139786 };
+  const destination = { latitude: 47.83942, longitude: 35.130647 };
+
+  return (
+    <View>
+      <MapView
+        style={{
+          height: "100%",
+          wedth: "100%",
+        }}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 47.746931,
+          longitude: 35.141517,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={2}
+          strokeColor="blue"
+        />
+        <Marker coordinate={origin} title={"Origin"} />
+        <Marker coordinate={destination} title={"Origin"} />
+      </MapView>
+    </View>
+  );
+};
+
+export default RouteMap;
--- src/screens/SearchResults/index.jsx
+++ src/screens/SearchResults/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { View, Text, Dimensions } from "react-native";
+import { View, Dimensions } from "react-native";
 
-import HomeMap from "../../components/HomeMap";
+import RouteMap from "../../components/RouteMap";
 import UberTypes from "../../components/UberTypes";
+
+
 
 const SearchResults = () => {
   return (
     <View style={{ display: "flex" }}>
       <View style={{ height: Dimensions.get("window").height - 350 }}>
-        <HomeMap />
+        <RouteMap />
       </View>
       <View style={{ height: 500 }}>
         <UberTypes />
