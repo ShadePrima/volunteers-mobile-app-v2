5f903341eedd4467781b50187f2263667a697a7e
ShadePrima <aleksandrag1984@gmail.com>
2022-08-25 11:49:50 +0300

    add new component NewNavigation

--- src/navigation/root.js
+++ src/navigation/root.js
@@ -1,24 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "../screens/HomeScreen";
-import DestinationSearch from "../screens/DestinationSearch";
-import SearchResults from "../screens/SearchResults";
+
+import HomeNavigation from "./Home";
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{ headerShown: false }}
-        initialRouteName={"Home"}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="DestinationSearch" component={DestinationSearch} />
-        <Stack.Screen name="SearchResults" component={SearchResults} />
-      </Stack.Navigator>
+      <HomeNavigation />
     </NavigationContainer>
   );
 };
