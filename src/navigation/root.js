305769eb731b5400efd394a6540b7080c5780c79
ShadePrima <aleksandrag1984@gmail.com>
2022-08-25 13:50:10 +0300

    add DummyScreen

--- src/navigation/root.js
+++ src/navigation/root.js
@@ -3,14 +3,32 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import HomeNavigation from "./Home";
+import { Text, View } from "react-native";
 
 const Drawer = createDrawerNavigator();
+
+const DummyScreen = (props) => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>{props.name}</Text>
+  </View>
+);
 
 const Navigation = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator>
         <Drawer.Screen name="Home" component={HomeNavigation} />
+        <Drawer.Screen name="Yuor trips">
+          {() => <DummyScreen name={"Yuor trips"} />}
+        </Drawer.Screen>
+
+        <Drawer.Screen name="Help">
+          {() => <DummyScreen name={"Help"} />}
+        </Drawer.Screen>
+
+        <Drawer.Screen name="Wallet">
+          {() => <DummyScreen name={"Wallet"} />}
+        </Drawer.Screen>
       </Drawer.Navigator>
     </NavigationContainer>
   );
